32c3dc0b8b60914be2202761498053ed57e68817
cornelius <macrosedk@gmail.com>
2025-03-22 00:00:37 +0100

    Lots of stuff

--- Scripts/Player.gd
+++ Scripts/Player.gd
@@ -1,12 +1,14 @@
 extends KinematicBody2D
 class_name Player
 
-
 export (int) var speed = 1200
 export (int) var jump_speed = -1800
 export (int) var gravity = 4000
 export (float, 0, 1.0) var friction = 0.1
 export (float, 0, 1.0) var acceleration = 0.25
+
+const PUSH_FORCE = 100
+const BLOCK_MAX_VELOCITY = 180
 
 var velocity = Vector2.ZERO
 
@@ -21,8 +23,17 @@ func get_input():
 
 func _physics_process(delta):
 	get_input()
-	velocity.y += gravity * delta
-	velocity = move_and_slide(velocity, Vector2.UP)
-	if Input.is_action_just_pressed("Jump"):
-		if is_on_floor():
+	if not is_on_floor():
+		velocity.y += gravity * delta
+
+	for i in get_slide_count():
+		var collision = get_slide_collision(i)
+		var collision_block = collision.get_collider()
+
+		if collision_block.is_in_group("Blocks") and abs(collision_block.get_linear_velocity().x) < BLOCK_MAX_VELOCITY:
+			collision_block.apply_central_impulse(collision.get_normal() * -PUSH_FORCE)
+
+	velocity = move_and_slide(velocity, Vector2.UP, false, 4, PI/4, false)
+
+	if Input.is_action_just_pressed("Jump") and is_on_floor():
 			velocity.y = jump_speed
